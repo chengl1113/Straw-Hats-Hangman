da54c0a0cf715595bb588ec49017820114ad28a0
mikewm2001 <michaelwmei09@gmail.com>
2024-02-13 04:28:01 -0500

    Added creation of textViews

--- app/src/main/java/com/example/strawhatshangman/MainActivity.kt
+++ app/src/main/java/com/example/strawhatshangman/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.Button
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
