e46f3347e6df65dc95e9ae33ff30232de75d1fae
mikewm2001 <michaelwmei09@gmail.com>
2024-02-13 04:24:53 -0500

    Added creation of textViews

--- app/src/main/java/com/example/strawhatshangman/MainActivity.kt
+++ app/src/main/java/com/example/strawhatshangman/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.example.strawhatshangman
 
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private val letterButtons: MutableList<Button> = mutableListOf()
-    private val wordBank: Map<String, List<String>> = mapOf(
-        "fruit" to listOf("apple", "banana", "orange", "mango"),
-        "drinks" to listOf("pepsi", "sprite", "fanta", "coke"),
-        "food" to listOf("pizza", "hotdog", "burger", "sandwich"),
-        "cars" to listOf("audi", "toyota", "mercedes", "lexus")
+    private val wordBank: Set<Pair<String, String>> = setOf(
+        "fruit" to "apple",
+        "fruit" to "banana",
+        "fruit" to "orange",
+        "fruit" to "mango",
+        "drinks" to "pepsi",
+        "drinks" to "sprite",
+        "drinks" to "fanta",
+        "drinks" to "coke",
+        "food" to "pizza",
+        "food" to "hotdog",
+        "food" to "burger",
+        "food" to "sandwich",
+        "cars" to "audi",
+        "cars" to "toyota",
+        "cars" to "mercedes",
+        "cars" to "lexus"
     )
+
     @SuppressLint("DiscouragedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +54,40 @@ class MainActivity : AppCompatActivity() {
 
         // Map all letter buttons to letterButtonClickListener
         letterButtons.map { button -> button.setOnClickListener(letterButtonClickListener) }
+
+        val randomElement: Pair<String, String> = wordBank.toList().random()
+
+        // Use this variable to reveal when user wants hint
+        val category : String = randomElement.first
+
+        // Use this variable to compare with user input(button clicks)
+        val word : String = randomElement.second
+
+
+        for (char in word) {
+            val textView = TextView(this)
+            textView.text = ""
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+            )
+            layoutParams.weight = 1f
+            textView.layoutParams = layoutParams
+            textView.textSize = 36f
+            textView.paintFlags = textView.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+//            textView.visibility = View.INVISIBLE
+            textView.setBackgroundResource(R.drawable.bottom_border)
+
+            val wordReveal : LinearLayout = findViewById(R.id.word_reveal)
+
+
+            // Add the TextView to layout
+
+            wordReveal.addView(textView)
+        }
+
+
+
     }
 
 
