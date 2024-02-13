a174075fc73031f0aaeaa371ce0fa57c89d0d17c
cheng <chengl@bu.edu>
2024-02-13 03:05:27 -0500

    added disappearing functionality to letter buttons

--- app/src/main/java/com/example/strawhatshangman/MainActivity.kt
+++ app/src/main/java/com/example/strawhatshangman/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.strawhatshangman
+
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.util.Log
-import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+
+    private val letterButtons: MutableList<Button> = mutableListOf()
+    @SuppressLint("DiscouragedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_keyboard)
+        setContentView(R.layout.activity_main)
+
+        // Add all buttons to letterButtons
+        for (i in 65..90) { // Change the range as needed
+            val buttonId = resources.getIdentifier("LETTER_"+i.toChar(),"id", packageName)
+            val button = findViewById<Button>(buttonId)
+            letterButtons.add(button)
+        }
+
+        val letterButtonClickListener = View.OnClickListener { view ->
+            val buttonPressed: Button = view as Button
+            buttonPressed.animate().alpha(0f).withEndAction {
+                buttonPressed.isEnabled = false
+            }
+        }
+
+        letterButtons.map { button -> button.setOnClickListener(letterButtonClickListener) }
     }
 }
