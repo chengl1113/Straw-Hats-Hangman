db09877a44d013e3d52a8356ea5fcf0ebb53dbdc
mikewm2001 <michaelwmei09@gmail.com>
2024-02-13 03:00:09 -0500

    Added dependencies

--- app/src/main/java/com/example/strawhatshangman/MainActivity.kt
+++ app/src/main/java/com/example/strawhatshangman/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.strawhatshangman
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
